eabe0e37a1b8df96bc4e15eee16f567faa63667a
dudes-come <dudes@qq.com>
2017-07-04 16:48:41 +0800

    add api controllers

---
+++ JoyOI.ManagementService.WebApi/Controllers/ActorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoyOI.ManagementService.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ActorController : Controller
+    {
+        /// <summary>
+        /// api/v1/Actor
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            await Task.Delay(1);
+            return new[] { "a", "b", "c" };
+        }
+    }
+}
---
+++ JoyOI.ManagementService.WebApi/Controllers/StateMachineController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoyOI.ManagementService.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class StateMachineController : Controller
+    {
+    }
+}
