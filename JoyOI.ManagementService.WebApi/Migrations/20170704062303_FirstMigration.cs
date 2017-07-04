c7a4cfb099b1bcce2db7571f147c0cd10213bd59
dudes-come <dudes@qq.com>
2017-07-04 14:23:34 +0800

    regenerate ef migration

---
+++ JoyOI.ManagementService.WebApi/Migrations/20170704062303_FirstMigration.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JoyOI.ManagementService.WebApi.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    Body = table.Column<string>(nullable: true),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    UpdateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                    table.UniqueConstraint("AK_Actors_Name", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blobs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    BlobId = table.Column<Guid>(nullable: false),
+                    Body = table.Column<byte[]>(nullable: true),
+                    ChunkIndex = table.Column<int>(nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    UpdateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blobs", x => x.Id);
+                    table.UniqueConstraint("AK_Blobs_BlobId_ChunkIndex", x => new { x.BlobId, x.ChunkIndex });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StateMachine",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    Body = table.Column<string>(nullable: true),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    UpdateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StateMachine", x => x.Id);
+                    table.UniqueConstraint("AK_StateMachine_Name", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StateMachineInstances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    CurrentActor = table.Column<JsonObject<ActorInfo>>(nullable: true),
+                    CurrentContainer = table.Column<string>(nullable: true),
+                    CurrentNode = table.Column<string>(nullable: true),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    FinishedActors = table.Column<JsonObject<ActorInfo[]>>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StateMachineInstances", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blobs_BlobId",
+                table: "Blobs",
+                column: "BlobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StateMachineInstances_Name",
+                table: "StateMachineInstances",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StateMachineInstances_Name_Status",
+                table: "StateMachineInstances",
+                columns: new[] { "Name", "Status" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Blobs");
+
+            migrationBuilder.DropTable(
+                name: "StateMachine");
+
+            migrationBuilder.DropTable(
+                name: "StateMachineInstances");
+        }
+    }
+}
---
+++ JoyOI.ManagementService.WebApi/Migrations/JoyOIManagementContextModelSnapshot.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using JoyOI.ManagementService.DbContexts;
+using JoyOI.ManagementService.Model.Enums;
+
+namespace JoyOI.ManagementService.WebApi.Migrations
+{
+    [DbContext(typeof(JoyOIManagementContext))]
+    partial class JoyOIManagementContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("JoyOI.ManagementService.Model.Entities.ActorEntity", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Body");
+
+                    b.Property<DateTime>("CreateTime");
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.Property<DateTime>("UpdateTime");
+
+                    b.HasKey("Id");
+
+                    b.HasAlternateKey("Name");
+
+                    b.ToTable("Actors");
+                });
+
+            modelBuilder.Entity("JoyOI.ManagementService.Model.Entities.BlobEntity", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<Guid>("BlobId");
+
+                    b.Property<byte[]>("Body");
+
+                    b.Property<int>("ChunkIndex");
+
+                    b.Property<DateTime>("CreateTime");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("UpdateTime");
+
+                    b.HasKey("Id");
+
+                    b.HasAlternateKey("BlobId", "ChunkIndex");
+
+                    b.HasIndex("BlobId");
+
+                    b.ToTable("Blobs");
+                });
+
+            modelBuilder.Entity("JoyOI.ManagementService.Model.Entities.StateMachineEntity", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Body");
+
+                    b.Property<DateTime>("CreateTime");
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.Property<DateTime>("UpdateTime");
+
+                    b.HasKey("Id");
+
+                    b.HasAlternateKey("Name");
+
+                    b.ToTable("StateMachine");
+                });
+
+            modelBuilder.Entity("JoyOI.ManagementService.Model.Entities.StateMachineInstanceEntity", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<JsonObject<ActorInfo>>("CurrentActor");
+
+                    b.Property<string>("CurrentContainer");
+
+                    b.Property<string>("CurrentNode");
+
+                    b.Property<DateTime>("EndTime");
+
+                    b.Property<JsonObject<ActorInfo[]>>("FinishedActors");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("StartTime");
+
+                    b.Property<int>("Status");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("Name");
+
+                    b.HasIndex("Name", "Status");
+
+                    b.ToTable("StateMachineInstances");
+                });
+        }
+    }
+}
