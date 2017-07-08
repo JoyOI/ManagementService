cd377738b4f94c839c2a2b8124824f3c24fe1228
あまみや ゆうこ <1@1234.sh>
2017-07-08 11:26:59 +0800

    Add sample statemachine

--- JoyOI.ManagementService.Playground/StateMachine.cs
+++ JoyOI.ManagementService.Playground/StateMachine.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.EntityFrameworkCore.Migrations;
 using System.Threading.Tasks;
 using System.Linq;
@@ -10,20 +11,162 @@ namespace JoyOI.ManagementService.Playground
 {
     public class StateMachine : StateMachineBase
     {
-        public override async Task RunAsync(string stage)
+        public static Regex InputFileRegex = new Regex("input_[0-9]{0,}.txt");
+
+        public override async Task RunAsync(string stage = "Start")
         {
             switch (stage)
             {
                 case "Start":
-                    goto case "CompileUserCode";
-                case "CompileUserCode":
-                    await SetStage("CompileUserCode");
-                    await DeployAndRunActorAsync(new RunActorParam("CompileUserCodeActor", InitialBlobs));
-                    goto case "RunUserCode";
-                case "RunUserCode":
-                    await SetStage("RunUserCode");
-                    var compileActorInfo = FindSingleActor(actor: "CompileUserCodeActor");
-                    await DeployAndRunActorAsync(new RunActorParam("RunUserCodeActor", compileActorInfo.Outputs));
+                    await SetStageAsync("Start");
+                    await DeployAndRunActorsAsync(
+                        new RunActorParam("CompileUserCodeActor", InitialBlobs.FindBlob("Main.cpp")), 
+                        new RunActorParam("CompileValidatorCodeActor", InitialBlobs.FindBlob("Validator.cpp")));
+                    goto case "ValidateUserCompileResult";
+                case "ValidateUserCompileResult":
+                    await SetStageAsync("ValidateUserCompileResult");
+                    var json = await StartedActors.FindSingleActor("Start", "CompileUserCodeActor").Outputs.FindBlob("runner.json").ReadAsJsonAsync<dynamic>();
+                    if (json.ExitCode != 0)
+                    {
+                        if (json.IsTimeout)
+                        {
+                            await HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Compile Error",
+                                Error = "Compiler timeout.",
+                                TimeUsed = json.UserTime
+                            });
+                        }
+                        else
+                        {
+                            await HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Compile Error",
+                                Error = StartedActors
+                                    .FindSingleActor("Start", "CompileUserCodeActor")
+                                    .Outputs
+                                    .FindBlob("stderr.txt"),
+                                ExitCode = json.ExitCode
+                            });
+                        }
+                        break;
+                    }
+                    goto case "ValidateValidatorCompileResult";
+                case "ValidateValidatorCompileResult":
+                    await SetStageAsync("ValidateValidatorCompileResult");
+                    var json2 = await StartedActors
+                        .FindSingleActor("Start", "CompileValidatorCodeActor")
+                        .Outputs
+                        .FindBlob("runner.json")
+                        .ReadAsJsonAsync<dynamic>();
+                    if (json2.ExitCode != 0)
+                    {
+                        if (json2.IsTimeout)
+                        {
+                            await HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Validator Compile Error",
+                                Error = "Compiler timeout.",
+                                TimeUsed = json2.UserTime
+                            });
+                        }
+                        else
+                        {
+                            await HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Validator Compile Error",
+                                Error = await StartedActors
+                                    .FindSingleActor("Start", "CompileValidatorCodeActor")
+                                    .Outputs
+                                    .FindBlob("stderr.txt")
+                                    .ReadAllTextAsync(),
+                                ExitCode = json2.ExitCode
+                            });
+                        }
+                        break;
+                    }
+                    goto case "RunUserProgramActor";
+                case "RunUserProgramActor":
+                    await SetStageAsync("RunUserProgramActor");
+                    var compileUserCodeActor = StartedActors.FindSingleActor("Start", "CompileUserCodeActor");
+                    var json3 = await compileUserCodeActor.Outputs.FindBlob("runner.json").ReadAsJsonAsync<dynamic>();
+                    if (json3.ExitCode != 0)
+                    {
+                        if (json3.IsTimeout)
+                        {
+                            await HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Compile Error",
+                                Error = "Compiler timeout.",
+                                TimeUsed = json3.UserTime
+                            });
+                        }
+                        else
+                        {
+                            await HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Compile Error",
+                                Error = await compileUserCodeActor
+                                    .Outputs
+                                    .FindBlob("stderr.txt").ReadAllTextAsync(),
+                                ExitCode = json3.ExitCode
+                            });
+                        }
+                        break;
+                    }
+                    var runActorParams = new List<RunActorParam>();
+                    var inputs = InitialBlobs.Where(x => InputFileRegex.IsMatch(x.Name));
+                    var userProgram = StartedActors.FindSingleActor("Start", "CompileUserCodeActor").Outputs.FindBlob("Main.out");
+                    foreach (var x in inputs)
+                    {
+                        runActorParams.Add(new RunActorParam("RunUserProgramActor", x, userProgram));
+                    }
+                    await DeployAndRunActorsAsync(runActorParams.ToArray());
+                    goto case "ValidateUserOutput";
+                case "ValidateUserOutput":
+                    await SetStageAsync("ValidateUserOutput");
+                    var runUserProgramActors = StartedActors.FindActor("RunUserProgramActor");
+                    var tasks4 = new List<Task>();
+                    foreach (var x in runUserProgramActors)
+                    {
+                        var json4 = await x.Outputs.FindBlob("runner.json").ReadAsJsonAsync<dynamic>();
+                        if (json4.ExitCode != 0)
+                        {
+                            tasks4.Add(HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = json4.IsTimeout ? "Time Limit Exceeded" : "Runtime Error",
+                                InputFile = x.Inputs.Single(y => InputFileRegex.IsMatch(y.Name))
+                            }));
+                        }
+                        else
+                        {
+                            var answerFilename = x.Outputs.Single(y => InputFileRegex.IsMatch(y.Name)).Name.Replace("input_", "output_");
+                            var answer = InitialBlobs.FindBlob(answerFilename);
+                            var stdout = x.Outputs.Single(y => y.Name == "stdout.txt");
+                            var validator = StartedActors
+                                .FindSingleActor("Start", "CompileValidatorCodeActor")
+                                .Outputs
+                                .FindBlob("Validator.out");
+                            tasks4.Add(DeployAndRunActorAsync(new RunActorParam("CompareActor", new[] { answer, stdout, validator })));
+                        }
+                    }
+                    await Task.WhenAll(tasks4);
+                    goto case "Finally";
+                case "Finally":
+                    await SetStageAsync("Finally");
+                    var actors5 = StartedActors.FindActor("ValidateUserOutput", "CompareActor");
+                    var tasks5 = new List<Task>();
+                    foreach (var x in actors5)
+                    {
+                        var json5 = await x.Outputs.FindBlob("runner.json").ReadAsJsonAsync<dynamic>();
+                        tasks5.Add(HttpInvokeAsync("PUT", "/JudgeResult/" + Id, new
+                        {
+                            Result = json5.ExitCode == 0 ? "Accepted" : (json5.ExitCode == 1 ? "Wrong Answer" : (json5.ExitCode == 2 ? "Presentation Error" : "Validator Error")),
+                            TimeUsed = json5.UserTime,
+                            MemoryUsed = json5.PeakMemory
+                        }));
+                    }
+                    await Task.WhenAll(tasks5);
                     break;
             }
         }
---
+++ JoyOI.ManagementService/Core/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Newtonsoft.Json;
+
+namespace JoyOI.ManagementService.Core
+{
+    public static class Extensions
+    {
+        public static Task<byte[]> ReadAllBytesAsync(this BlobInfo self)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<string> ReadAllTextAsync(this BlobInfo self)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task<T> ReadAsJsonAsync<T>(this BlobInfo self) => JsonConvert.DeserializeObject<T>(await self.ReadAllTextAsync());
+
+        public static IEnumerable<ActorInfo> FindActor(this IEnumerable<ActorInfo> self, string stage = null, string actor = null)
+        {
+            if (stage != null)
+            {
+                self = self.Where(x => x.Stage == stage);
+            }
+
+            if (actor != null)
+            {
+                self = self.Where(x => x.Name == actor);
+            }
+
+            return self;
+        }
+
+        public static ActorInfo FindSingleActor(this IEnumerable<ActorInfo> self, string stage = null, string actor = null)
+        {
+            return self.FindActor(stage, actor).Single();
+        }
+
+        public static BlobInfo FindBlob(this IEnumerable<BlobInfo> self, string filename) => self.SingleOrDefault(x => x.Name == filename);
+
+        public static BlobInfo FindInputBlob(this ActorInfo self, string filename) => self.Inputs.SingleOrDefault(x => x.Name == filename);
+
+        public static BlobInfo FindOutputBlob(this ActorInfo self, string filename) => self.Outputs.SingleOrDefault(x => x.Name == filename);
+    }
+}
