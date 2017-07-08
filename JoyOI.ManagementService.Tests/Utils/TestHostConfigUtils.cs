cff22fdd7be96ce919716bbe3d2d048bace44d26
dudes-come <dudes@qq.com>
2017-07-08 15:04:30 +0800

    add utils tests

---
+++ JoyOI.ManagementService.Tests/Utils/TestHostConfigUtils.cs
@@ -0,0 +1,38 @@
+﻿using Docker.DotNet.Models;
+using JoyOI.ManagementService.Utils;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JoyOI.ManagementService.Tests.Utils
+{
+    public class TestHostConfigUtils
+    {
+        [Fact]
+        public void WithLimitation()
+        {
+            var hostConfig = new HostConfig();
+            hostConfig = HostConfigUtils.WithLimitation(hostConfig, new ContainerLimitation()
+            {
+                CPUPeriod = 123,
+                CPUQuota = 321,
+                Memory = 888
+            });
+            Assert.Equal(123, hostConfig.CPUPeriod);
+            Assert.Equal(321, hostConfig.CPUQuota);
+            Assert.Equal(888, hostConfig.Memory);
+            hostConfig = HostConfigUtils.WithLimitation(hostConfig, new ContainerLimitation()
+            {
+                MemorySwap = 168,
+                StorageBaseSize = 8
+            });
+            Assert.Equal(123, hostConfig.CPUPeriod);
+            Assert.Equal(321, hostConfig.CPUQuota);
+            Assert.Equal(888, hostConfig.Memory);
+            Assert.Equal(168, hostConfig.MemorySwap);
+            Assert.Equal("8G", hostConfig.StorageOpt["dm.basesize"]);
+        }
+    }
+}
---
+++ JoyOI.ManagementService.Tests/Utils/TestRandomUtils.cs
@@ -0,0 +1,18 @@
+﻿using JoyOI.ManagementService.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JoyOI.ManagementService.Tests.Utils
+{
+    public class TestRandomUtils
+    {
+        [Fact]
+        public void GetRandomInstance()
+        {
+            Assert.True(RandomUtils.GetRandomInstance() != null);
+            Assert.Equal(RandomUtils.GetRandomInstance(), RandomUtils.GetRandomInstance());
+        }
+    }
+}
