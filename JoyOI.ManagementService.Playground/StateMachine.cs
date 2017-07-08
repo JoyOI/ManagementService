9637f774192ddb2daff66f6226f78fb2a6b31f93
あまみや ゆうこ <1@1234.sh>
2017-07-08 11:36:29 +0800

    Added mle verification

--- JoyOI.ManagementService.Playground/StateMachine.cs
+++ JoyOI.ManagementService.Playground/StateMachine.cs
@@ -12,9 +12,13 @@ namespace JoyOI.ManagementService.Playground
     public class StateMachine : StateMachineBase
     {
         public static Regex InputFileRegex = new Regex("input_[0-9]{0,}.txt");
+        public static Regex OutputFileRegex = new Regex("output_[0-9]{0,}.txt");
 
         public override async Task RunAsync(string stage = "Start")
         {
+            // Prepare
+            var profile = await InitialBlobs.FindBlob("profile.json").ReadAsJsonAsync<dynamic>();
+
             switch (stage)
             {
                 case "Start":
@@ -130,7 +134,15 @@ namespace JoyOI.ManagementService.Playground
                     foreach (var x in runUserProgramActors)
                     {
                         var json4 = await x.Outputs.FindBlob("runner.json").ReadAsJsonAsync<dynamic>();
-                        if (json4.ExitCode != 0)
+                        if (json4.PeakMemory > profile.Memory)
+                        {
+                            tasks4.Add(HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
+                            {
+                                Result = "Memory Limit Exceeded",
+                                InputFile = x.Inputs.Single(y => InputFileRegex.IsMatch(y.Name))
+                            }));
+                        }
+                        else if (json4.ExitCode != 0)
                         {
                             tasks4.Add(HttpInvokeAsync("PUT", "/JudgeResult/" + this.Id, new
                             {
@@ -154,16 +166,17 @@ namespace JoyOI.ManagementService.Playground
                     goto case "Finally";
                 case "Finally":
                     await SetStageAsync("Finally");
-                    var actors5 = StartedActors.FindActor("ValidateUserOutput", "CompareActor");
+                    var compileActors = StartedActors.FindActor("ValidateUserOutput", "CompareActor");
                     var tasks5 = new List<Task>();
-                    foreach (var x in actors5)
+                    foreach (var x in compileActors)
                     {
                         var json5 = await x.Outputs.FindBlob("runner.json").ReadAsJsonAsync<dynamic>();
                         tasks5.Add(HttpInvokeAsync("PUT", "/JudgeResult/" + Id, new
                         {
                             Result = json5.ExitCode == 0 ? "Accepted" : (json5.ExitCode == 1 ? "Wrong Answer" : (json5.ExitCode == 2 ? "Presentation Error" : "Validator Error")),
                             TimeUsed = json5.UserTime,
-                            MemoryUsed = json5.PeakMemory
+                            MemoryUsed = json5.PeakMemory,
+                            InputFile = x.Inputs.Single(y => OutputFileRegex.IsMatch(y.Name)).Name.Replace("output_", "input_")
                         }));
                     }
                     await Task.WhenAll(tasks5);
