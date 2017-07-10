5d067a2b815d1e6b830f0f3a2cd39810eaad261d
dudes-come <dudes@qq.com>
2017-07-10 13:46:46 +0800

    add simple state machine

---
+++ JoyOI.ManagementService.Playground/Simple/SimpleStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using JoyOI.ManagementService.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Threading.Tasks;
+using System.Linq;
+
+/*
+测试参数:
+{
+  "name": "CompileAndRun",
+  "initialBlobs": [
+    {
+      "id": "填写blob.id",
+      "name": "Main.c"
+    }
+  ]
+}
+*/
+
+namespace JoyOI.ManagementService.Playground
+{
+    public class SimpleStateMachine : StateMachineBase
+    {
+        public override async Task RunAsync()
+        {
+            switch (Stage)
+            {
+                case "Start":
+                    goto case "CompileUserCode";
+                case "CompileUserCode":
+                    await SetStageAsync("CompileUserCode");
+                    await DeployAndRunActorAsync(new RunActorParam("CompileUserCodeActor", InitialBlobs));
+                    goto case "RunUserCode";
+                case "RunUserCode":
+                    await SetStageAsync("RunUserCode");
+                    var compileActorInfo = StartedActors.FindSingleActor(actor: "CompileUserCodeActor");
+                    await DeployAndRunActorAsync(new RunActorParam("RunUserCodeActor", compileActorInfo.Outputs));
+                    break;
+            }
+        }
+    }
+}
