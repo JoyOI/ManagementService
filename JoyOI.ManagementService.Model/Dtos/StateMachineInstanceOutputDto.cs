3e7c8c06d5fb19a00fe5ddaa9edb1e8482854676
dudes-come <dudes@qq.com>
2017-07-11 09:34:24 +0800

    update dto

--- JoyOI.ManagementService.Model/Dtos/StateMachineInstanceOutputDto.cs
+++ JoyOI.ManagementService.Model/Dtos/StateMachineInstanceOutputDto.cs
@@ -12,13 +12,14 @@ namespace JoyOI.ManagementService.Model.Dtos
         public Guid Id { get; set; }
         public string Name { get; set; }
         public StateMachineStatus Status { get; set; }
-        public IList<ActorInfo> FinishedActors { get; set; }
-        public ActorInfo CurrentActor { get; set; }
+        public string Stage { get; set; }
+        public IList<ActorInfo> StartedActors { get; set; }
+        public IList<BlobInfo> InitialBlobs { get; set; }
         public ContainerLimitation Limitation { get; set; }
-        public string CurrentNode { get; set; }
-        public string CurrentContainer { get; set; }
         public string FromManagementService { get; set; }
         public int ReRunTimes { get; set; }
+        public string Exception { get; set; }
+        public string ExecutionKey { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime? EndTime { get; set; }
     }
