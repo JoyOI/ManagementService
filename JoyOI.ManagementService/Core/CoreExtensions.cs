fbe778fa83aa0f219f0f64c6c0f38a2670964b76
dudes-come <dudes@qq.com>
2017-07-19 08:45:01 +0800

    improve core extensions (reduce null exceptions)

--- JoyOI.ManagementService/Core/CoreExtensions.cs
+++ JoyOI.ManagementService/Core/CoreExtensions.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Newtonsoft.Json;
-using JoyOI.ManagementService.Services;
 
 namespace JoyOI.ManagementService.Core
 {
@@ -13,18 +12,24 @@ namespace JoyOI.ManagementService.Core
     {
         public static async Task<byte[]> ReadAllBytesAsync(this BlobInfo self, StateMachineBase stm)
         {
+            if (self == null)
+                throw new ArgumentNullException("self");
             var blobs = await stm.Store.ReadBlobs(new[] { self });
             return blobs.First().Item2;
         }
 
         public static async Task<string> ReadAllTextAsync(this BlobInfo self, StateMachineBase stm)
         {
+            if (self == null)
+                throw new ArgumentNullException("self");
             var blobs = await stm.Store.ReadBlobs(new[] { self });
             return Encoding.UTF8.GetString(blobs.First().Item2);
         }
 
         public static async Task<T> ReadAsJsonAsync<T>(this BlobInfo self, StateMachineBase stm)
         {
+            if (self == null)
+                throw new ArgumentNullException("self");
             return JsonConvert.DeserializeObject<T>(await self.ReadAllTextAsync(stm));
         }
 
@@ -45,7 +50,10 @@ namespace JoyOI.ManagementService.Core
 
         public static ActorInfo FindSingleActor(this IEnumerable<ActorInfo> self, string stage = null, string actor = null)
         {
-            return self.FindActor(stage, actor).Single();
+            var actorInfo = self.FindActor(stage, actor).SingleOrDefault();
+            if (actorInfo == null)
+                throw new KeyNotFoundException($"find single actor failed: stage is \"{stage}\", actor is \"{actor}\"");
+            return actorInfo;
         }
 
         public static BlobInfo FindBlob(this IEnumerable<BlobInfo> self, string filename)
@@ -61,6 +69,30 @@ namespace JoyOI.ManagementService.Core
         public static BlobInfo FindOutputBlob(this ActorInfo self, string filename)
         {
             return self.Outputs.SingleOrDefault(x => x.Name == filename);
+        }
+
+        public static BlobInfo FindSingleBlob(this IEnumerable<BlobInfo> self, string filename)
+        {
+            var blobInfo = FindBlob(self, filename);
+            if (blobInfo == null)
+                throw new KeyNotFoundException($"find single blob failed: filename is \"{filename}\"");
+            return blobInfo;
+        }
+
+        public static BlobInfo FindSingleInputBlob(this ActorInfo self, string filename)
+        {
+            var blobInfo = self.Inputs.SingleOrDefault(x => x.Name == filename);
+            if (blobInfo == null)
+                throw new KeyNotFoundException($"find single input blob failed: filename is \"{filename}\"");
+            return blobInfo;
+        }
+
+        public static BlobInfo FindSingleOutputBlob(this ActorInfo self, string filename)
+        {
+            var blobInfo = self.Outputs.SingleOrDefault(x => x.Name == filename);
+            if (blobInfo == null)
+                throw new KeyNotFoundException($"find single output blob failed: filename is \"{filename}\"");
+            return blobInfo;
         }
     }
 }
