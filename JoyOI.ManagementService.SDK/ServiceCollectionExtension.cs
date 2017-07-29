d2c01b327eeb8edef8abb4f23cf6ca64715013f3
あまみや ゆうこ <1@1234.sh>
2017-07-29 19:11:04 +0800

    Added di ext

---
+++ JoyOI.ManagementService.SDK/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using JoyOI.ManagementService.SDK;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddJoyOIManagementService(this IServiceCollection self)
+        {
+            return self.AddSingleton<ManagementServiceClient>();
+        }
+    }
+}
